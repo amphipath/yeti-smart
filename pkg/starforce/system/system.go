8d4ec883cdbbb29d76dd65897d97127113747eca
amphipath <amphipath@users.noreply.github.com>
2024-05-26 17:20:49 +0800

    add system interface

---
+++ pkg/starforce/system/system.go
@@ -0,0 +1,29 @@
+package starforce
+
+type (
+	StarforceSystem interface {
+		GetAttemptCost(itemLevel int, currentStar int) int
+		GetProbabilities(currentStar int) (float64, float64, float64, float64)
+		GetMaxStar() int
+	}
+
+	sfSystem struct {
+		Probabilities []MatrixRow
+		CostFunction  func(itemLevel, currentStar int) int
+	}
+)
+
+// GetBaseProbabilities returns the base success, maintain, drop, boom chances in the starforce system assuming no events or star catch
+func (s *sfSystem) GetBaseProbabilities(currentStar int) (float64, float64, float64, float64) {
+	r := s.Probabilities[currentStar]
+	return r.Success, 1 - r.Boom - r.Drop - r.Success, r.Drop, r.Boom
+}
+
+// GetBaseAttemptCost returns the base cost attempt for a given item level and star without any events or antiboom
+func (s *sfSystem) GetBaseAttemptCost(itemLevel int, currentStar int) int {
+	return s.CostFunction(itemLevel, currentStar)
+}
+
+func (s *sfSystem) GetMaxStar() int {
+	return len(s.Probabilities)
+}
