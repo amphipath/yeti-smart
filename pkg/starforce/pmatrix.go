1852f913c3e550394fb8ab2767ca8b11016922ab
amphipath <amphipath@users.noreply.github.com>
2024-05-26 20:40:09 +0800

    comment about what the MatrixRow struct is for

--- pkg/starforce/pmatrix.go
+++ pkg/starforce/pmatrix.go
@@ -1,6 +1,7 @@
 package starforce
 
 type (
+	// MatrixRow describes the base data type that describes the base outcome chances of a star attempt per star per sf system
 	MatrixRow struct {
 		Success float64
 		Drop    float64
